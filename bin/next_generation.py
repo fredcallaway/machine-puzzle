8a28fd40c634f606fd0135b36a7caf98b004552a
fredcallaway <fredc@llaway.com>
2024-05-20 11:00:59 -0700

    next_generation script

---
+++ bin/next_generation.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import subprocess
+import re
+
+def bash(cmd):
+    status, output = subprocess.getstatusoutput(cmd)
+    if status != 0:
+        print("Problem with command:", cmd)
+        print(output)
+        raise Exception()
+    return output
+
+with open('config.txt') as f:
+    config = f.read()
+
+codeversion = re.search(r'experiment_code_version = (.*)', config).group(1)
+prev_gen = int(codeversion.split('g')[1])
+next_gen = prev_gen + 1
+
+if prev_gen != 0:
+    print('fetch data')
+    out = bash('bin/fetch_data.py')
+    n = int(re.search(r'(\d+) complete', out).group(1))
+    if n < 10:
+        print(f'{n} participants completed; try again later.')
+        exit(1)
+    print(bash('bin/prolific.py approve_all'))
+
+print('generate stimuli')
+bash(f'cd ../compositionality-model && jl generate_redblack.jl {next_gen}' )
+
+print('update code version')
+next_code = codeversion.split('g')[0] + 'g' + str(next_gen)
+with open('config.txt', 'w') as f:
+    f.write(config.replace(codeversion, next_code))
+
+# update heroku
+print('update heroku')
+bash('git add config.txt')
+bash('git add static/json')
+bash(f'git commit -m "{next_code}"')
+bash('git push heroku master')
+
+print(bash("bin/prolific.py post_duplicate --no_check"))
